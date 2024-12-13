c746e020d9721fef6ccac43a2884c15e645a889f
atipEG <144408952+atipEG@users.noreply.github.com>
2024-12-13 14:44:59 +0700

    Add project files.

---
+++ ReportsWebsiteTest/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=15.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        html, body, form {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        #form1 {
+            height: 100%;
+        }
+
+        #ReportViewer1 {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 100%;">
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+            <rsweb:ReportViewer ID="ReportViewer1" runat="server"
+                Font-Names="Verdana" Font-Size="8pt"
+                Width="793.7px" Height="1122.52px">
+                <LocalReport ReportPath="report1.rdlc">
+                </LocalReport>
+            </rsweb:ReportViewer>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ReportsWebsiteTest/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿//------------------------------------------------------------------
+// <copyright company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Reset the ReportViewer
+            ReportViewer1.Reset();
+            LocalReport report = ReportViewer1.LocalReport;
+
+            // Set the path to the RDLC file
+            report.ReportPath = Server.MapPath("~/report1.rdlc");
+
+            // Define A4 page dimensions and margins
+            const double a4Width = 8.27;  // A4 width in inches
+            const double a4Height = 11.69; // A4 height in inches
+            const double margin = 0.5;   // Margin in inches
+
+            // Dimensions for the TextBox
+            double textBoxWidth = 7.0;  // Width of the TextBox in inches
+            double textBoxHeight = 1.0; // Height of the TextBox in inches
+
+            // Calculate positions for centering the TextBox
+            double textBoxLeft = (a4Width - textBoxWidth) / 2; // Horizontal center
+            double textBoxTop = (a4Height - textBoxHeight) / 2; // Vertical center
+
+            // Pass these positions as parameters to the RDLC
+            ReportParameter[] parameters = new ReportParameter[]
+            {
+            new ReportParameter("TextBoxLeft", textBoxLeft.ToString("F2")), // Left position
+            new ReportParameter("TextBoxTop", textBoxTop.ToString("F2"))    // Top position
+            };
+
+            // Add the parameters to the report
+            report.SetParameters(parameters);
+
+            // Refresh the ReportViewer
+            ReportViewer1.LocalReport.Refresh();
+        }
+    }
+}
